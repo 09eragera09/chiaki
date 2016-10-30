d3d3a3b326c91927b48e66dd3a5dc81bd783de8d
Gaurav Gupta <gg873702@gmail.com>
2016-10-30 11:28:31 +0530

    Added Moderation Features such as ban, kick, mute and unmute

--- chiaki.py
+++ chiaki.py
@@ -4,12 +4,36 @@ import random
 import sys
 from time import time
 
+def getTime():
+    return time()
+
+def checkForAuth(message, perm):
+    auth = False
+    if message.author = message.server.owner or message.author.id = "94374744576512000":
+        auth = True
+
+    else:
+        for x in message.author.roles:
+            if perm in x.permissions:
+                auth = True
+                break
+    return auth
+
 client = discord.Client()
 
 global bot_startup
 
-def getTime():
-    return time()
+async def mute(member):
+    if not "muted" in [x.name for x in member.server.roles]:
+        await client.create_role(member.server, name="muted", permissions=discord.permissions.none())
+        await client.send_message(member.server.owner, "The `muted` role has been created for moderation purposes. Please push it up the list for more effective usage")
+    await.client.add_roles(member, [x for x in member.server.roles if x.name == "muted"])
+    silences[member.id] = [member.server.id, getTime()]
+
+async def unmute(member):
+    if not "muted" in [x.name for x in member.roles]:
+        await client.send_message(message.channel, "This user is not muted, so not unmuted")
+    await client.remove_roles(member, [x for x in member.roles if x.name == "muted"])
 
 def calculateTime(totalseconds):
     totalminutes = int(totalseconds/60)
@@ -56,25 +80,31 @@ async def on_message(message):
         'Concentrate and ask again', 'Don\'t count on it', 'My reply is no',
         'My sources say no', 'Outlook not so good', 'Very doubtful']
         await client.send_message(message.channel, random.choice(magicball))
+
     elif message.content.startswith('!ping'):
         await client.send_message(message.channel, 'Your net is working, %s-kun' % message.author.name)
+
     elif message.content.startswith('!malid'):
         tmp = message.content.split()
         if len(tmp) >= 2:
             await client.send_message(message.channel, 'Here\'s your MAL ID, http://myanimelist.net/animelist/%s' % tmp[1])
         else:
-            await client.send_message(message.channel, 'Please enter the right number of arguments and try again')
-    elif message.content.startswith('!kill'):
-        await client.send_message(message.channel, 'Chiaki will now exit.')
-        sys.exit()
+            await client.send_message(message.channel, 'Please enter a username')
+
+    elif message.content.startswith('!sleep'):
+        if message.author.id = "94374744576512000":
+            await client.send_message(message.channel, 'Era-kun, carry me to my bed')
+            sys.exit()
+        else:
+            await client.send_message(message.channel, "Mou! You're not Era-kun.")
+
     elif message.content.startswith('!urban'):
         tmp = message.content.split()
         if len(tmp) >= 2:
             await client.send_message(message.channel, 'http://www.urbandictionary.com/define.php?term=%s' % tmp[1])
         else:
             await client.send_message(message.channel, 'Please enter an argument.')
-    elif message.content.startswith('!help'):
-        await client.send_message(message.channel, 'This doesnt contain anything. _yet_')
+
     elif message.content.startswith('!uptime'):
         currentTime = getTime()
         seconds = int(currentTime - bot_startup)
@@ -82,18 +112,54 @@ async def on_message(message):
         hours = minutes / 60
         days = hours / 24
         if seconds < 60:
-            await client.send_message(message.channel, 'Chiaki has been up for **%d** seconds.' % seconds)
+            await client.send_message(message.channel, 'I have been up for **%d** seconds! Let\'s go to the arcade!' % seconds)
         elif seconds <= 3600:
             division = divide(seconds, 60)
-            await client.send_message(message.channel, 'Chiaki has been up for **%d** minutes and **%d** seconds' % (division[0], division[1]))
+            await client.send_message(message.channel, 'I have been up for **%d** minutes and **%d** seconds. I\'m bored.' % (division[0], division[1]))
         elif seconds <= 3600 * 24:
             division_h = divide(minutes, 60)
-            seconds = division_h[2] * 60
+            seconds = division_h[1] * 60
             division = divide(seconds, 60)
-            await client.send_message(message.channel, 'Chiaki has been up for **%d** hours, **%d** minutes and **%d** seconds.' % (division_h[0], division[0], division[1]))
+            await client.send_message(message.channel, 'I have been up for **%d** hours, **%d** minutes and **%d** seconds. Im sleepy....' % (division_h[0], division[0], division[1]))
         else:
             time = calculateTime(seconds)
-            await client.send_message(message.channel, 'Chiaki has been up for **%d** days, **%d** hours, **%d** minutes and **%d** seconds.' % (time[0], time[1], time[2], time[3]))
+            await client.send_message(message.channel, 'Chiaki has been up for **%d** days, **%d** hours, **%d** minutes and **%d** seconds. Oh, it seems she\'s sleeping while standing.' % (time[0], time[1], time[2], time[3]))
+
+    elif message.content.startswith('!ban'):
+        authorized = checkForAuth(message, ban_members)
+        await client.delete_message(message)
+        if authorized:
+            for member in stuff:
+                await ban(member)
+        else:
+            await client.send_message(message.channel, "You are not authorized to use the ban command.")
+
+    elif message.content.startswith('!kick'):
+        authorized = checkForAuth(message, kick_members)
+        await client.delete_message(message)
+        if authorized:
+            for memeber in message.mentions:
+                await kick(member)
+        else:
+            await client.send_message(message.channel, "You are not authorized to use the kick command")
+    elif message.content.startswith('!mute'):
+        authorized = checkForAuth(message, manage_roles)
+        if authorized:
+            for memeber in message.mentions:
+                await mute(member)
+
+    elif message.content.startswith('!unmute'):
+        authorized = checkForAuth(message, manage_roles)
+        if authorized:
+            for member in message.mentions:
+                await unmute(member)
+
+    elif message.content.startswith('!help'):
+        helptext = "There are a few commands you can use.\n`!ping` to check if your net is working ;)\n`!uptime` to check how long the bot has been up\n`!8ball` the magic 8ball will tell reply with either an affirmative, negative or a non-commital response\n`!urban` to check urbandictionary for the definition of a term\n`malid` to get your animelist from myanimelist\n\nCommands for Moderators\n`!ban`, `!kick`, `!mute`, and `!unmute`, do exactly what they say. Supply with Discord Username\n\nCommand for Era-kun only\n`!sleep`"
+        await client.send_message(message.channel, helptext)
+
+
+
 token = open('token', 'r').read()
 token = token.rstrip('\n')
 client.run(token)
