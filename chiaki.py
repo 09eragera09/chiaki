7bbf80dc1bc197037f51c117f7f53a19a4068a4e
0eragera09 <gg873702@gmail.com>
2016-12-21 09:56:01 +0530

    Stop being weird github

--- chiaki.py
+++ chiaki.py
@@ -434,3 +434,4 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 client.run(token)
+
